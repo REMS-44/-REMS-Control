30324cdf64a9eb1f431007f95740b1ca41f977b4
REMS-44 <vmfisherfisher@gmail.com>
2026-09-11 15:25:52 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'rems-control-shell-v42.0-large-forms';
+const CACHE = 'rems-control-shell-v42.1-large-forms-fix';
 const SHELL = ['./', './index.html', './styles.css', './responsive.css', './responsive.js'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(() => {})); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
